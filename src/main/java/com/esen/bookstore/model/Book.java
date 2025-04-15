c5773a2634c74e9aeeff5962f5ecdae8573315b8
SzollosBoglarka <szollosboglarka@gmail.com>
2025-04-15 09:35:48 +0200

    Database seeding

--- src/main/java/com/esen/bookstore/model/Book.java
+++ src/main/java/com/esen/bookstore/model/Book.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Data
+
 @Entity
 public class Book {
 
@@ -17,4 +17,54 @@ public class Book {
     private String author;
     private String publisher;
     private Double price;
+
+    public Book(){}
+
+    public Book(Long id, String title, String author, String publisher, Double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
