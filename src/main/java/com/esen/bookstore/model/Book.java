76596dc3a4515aaba7cddf2ccc016904eab6f754
SzollosBoglarka <szollosboglarka@gmail.com>
2025-04-22 09:35:18 +0200

    Start implementing shell command

--- src/main/java/com/esen/bookstore/model/Book.java
+++ src/main/java/com/esen/bookstore/model/Book.java
@@ -4,10 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 
 @Entity
+@Data
+@Builder
+@AllArgsConstructor
 public class Book {
 
     @Id
@@ -18,53 +23,4 @@ public class Book {
     private String publisher;
     private Double price;
 
-    public Book(){}
-
-    public Book(Long id, String title, String author, String publisher, Double price) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.publisher = publisher;
-        this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
 }
--- src/main/java/com/esen/bookstore/model/Bookstore.java
+++ src/main/java/com/esen/bookstore/model/Bookstore.java
@@ -1,11 +1,15 @@
 package com.esen.bookstore.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Map;
 
 @Entity
+@Data
+@AllArgsConstructor
 public class Bookstore {
 
     @Id
@@ -18,55 +22,7 @@ public class Bookstore {
     private Double moneyInCashRegister;
 
     @ElementCollection(fetch = FetchType.EAGER)
+    @JsonIgnore
     private Map<Book, Integer> inventory;
 
-    public Bookstore(){}
-
-    public Bookstore(Long id, String location, Double priceModifier, Double moneyInCashRegister, Map<Book, Integer> inventory) {
-        this.id = id;
-        this.location = location;
-        this.priceModifier = priceModifier;
-        this.moneyInCashRegister = moneyInCashRegister;
-        this.inventory = inventory;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Double getPriceModifier() {
-        return priceModifier;
-    }
-
-    public void setPriceModifier(Double priceModifier) {
-        this.priceModifier = priceModifier;
-    }
-
-    public Double getMoneyInCashRegister() {
-        return moneyInCashRegister;
-    }
-
-    public void setMoneyInCashRegister(Double moneyInCashRegister) {
-        this.moneyInCashRegister = moneyInCashRegister;
-    }
-
-    public Map<Book, Integer> getInventory() {
-        return inventory;
-    }
-
-    public void setInventory(Map<Book, Integer> inventory) {
-        this.inventory = inventory;
-    }
 }
